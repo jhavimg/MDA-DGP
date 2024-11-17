b4cf1c93ecaff0c63b775e0411c64c0e2d95597e
Jesús Marín Almansa <jesus@jmarin.dev>
2024-11-17 16:49:49 +0100

    [Add] Integrate CORS headers and add idTarea field to Tarea model and serializer

--- BackEnd/EspecialEduca/settings.py
+++ BackEnd/EspecialEduca/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_mongoengine",
+    "corsheaders",
     "app",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
