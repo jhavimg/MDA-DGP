1f26e256564f9d5ea2315c71e894b9727a79b29f
Alejandro Ruiz <alejandroruizsalazar21@gmail.com>
2024-11-26 10:59:07 +0100

    Tarea por pasos y test actualizados

--- otros/testCrearTareaPasos.py
+++ otros/testCrearTareaPasos.py
@@ -28,18 +28,25 @@ try:
     pictogram_path = os.path.join(os.path.dirname(__file__), 'test_pictogram.png')
     driver.find_element(By.NAME, 'pictogramas').send_keys(pictogram_path)
 
+    # Añadir un paso
+    driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/form/div/button').click()
+    driver.find_element(By.XPATH, '//*[@id="paso_nombre_0"]').send_keys('Leer el capítulo 1.')
+    driver.find_element(By.XPATH, '//*[@id="paso_descripcion_0"]').send_keys('Leer el capítulo 1 del libro.')
+
+
     time.sleep(5)
     # Enviar el formulario
     driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/form/a/button').click ()
+    time.sleep(2)
 
-    # Esperar a que la página se recargue
-    time.sleep(5)
-
+    driver.quit()
+    driver = webdriver.Chrome()
+    
     # Verificar que la tarea aparece en la lista
     driver.get('http://localhost:3000/Tarea_list')
     time.sleep(5)  # Esperar a que la página se cargue completamente
 
-    assert 'Tarea de Matemáticas' in driver.page_source
-
+    assert 'Tarea de Prueba' in driver.page_source
+    print('La tarea se creó correctamente')
 finally:
     driver.quit()
