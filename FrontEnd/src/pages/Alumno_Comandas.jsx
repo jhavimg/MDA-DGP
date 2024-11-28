1c7c28a58cf37691dbd85c7368ba3309cde6290e
Godoy <jhavimg@gmail.com>
2024-11-28 23:03:33 +0100

    correccion css en alumno_comandas

--- FrontEnd/src/pages/Alumno_Comandas.jsx
+++ FrontEnd/src/pages/Alumno_Comandas.jsx
@@ -12,18 +12,16 @@ function AlumnoComandas(props){
         <div className = "cuerpo_comandas">
             <div className="menu-container">
                 <h1>MENÚS</h1>
-            <div className="numbers-grid">
-                <MenuComp   image="../images/menu.png" alt = "1" />
-                <MenuComp   image ="../images/carne.png" alt = "2"/>
-                <MenuComp   image ="../images/sinCarne.png" alt = "3" />
-                <MenuComp   image ="../images/fruta.png" alt = "4" />
-                <MenuComp   image ="../images/trituradoSinCarne.png" alt = "5"/>
-                <MenuComp   image ="../images/triturado.png" alt = "6" />
-                <MenuComp   image ="../images/noLentejas.png" alt = "7" />
-                <MenuComp   image ="../images/yogurNatillas.png" alt = "8" />
-                
-                
-            </div>
+                <div className="numbers-grid">
+                    <MenuComp   image="../images/menu.png" alt = "1" />
+                    <MenuComp   image ="../images/carne.png" alt = "2"/>
+                    <MenuComp   image ="../images/sinCarne.png" alt = "3" />
+                    <MenuComp   image ="../images/fruta.png" alt = "4" />
+                    <MenuComp   image ="../images/trituradoSinCarne.png" alt = "5"/>
+                    <MenuComp   image ="../images/triturado.png" alt = "6" />
+                    <MenuComp   image ="../images/noLentejas.png" alt = "7" />
+                    <MenuComp   image ="../images/yogurNatillas.png" alt = "8" />
+                </div>
                 <button className="done-button" >Hecho ✔</button>
             </div>
         </div>
