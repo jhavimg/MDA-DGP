7fc217668929e493cfd106bbe44a05ad338df405
Jesús Marín Almansa <jesus@jmarin.dev>
2024-12-04 20:07:36 +0100

    Implementar gestión de accesibilidades en el perfil del alumno

--- FrontEnd/src/components/Accesibilidad.jsx
+++ FrontEnd/src/components/Accesibilidad.jsx
@@ -1,61 +1,84 @@
-import "../css/AForm.css"
+import "../css/AForm.css";
+import { useState, useEffect } from 'react';
 
-//Formulario para crear un alumno, se envía a la ruta /api/alumnos por el método POST
-function Accesibilidad(){
-    return(
+function Accesibilidad({ initialAccesibilidades, onAccesibilidadesChange }) {
+    const [accesibilidadesSeleccionadas, setAccesibilidadesSeleccionadas] = useState({});
+    const [accesibilidades, setAccesibilidades] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchAccesibilidades() {
+            try {
+                const response = await fetch('https://especialeduca.jmarin.dev/api/accesibilidades/');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                if (data.success && data.data) {
+                    setAccesibilidades(data.data);
+                    const initialSelection = {};
+                    data.data.forEach(item => {
+                        initialSelection[item.id] = false;
+                    });
+                    setAccesibilidadesSeleccionadas(initialAccesibilidades || initialSelection);
+
+                } else {
+                    throw new Error('Formato de datos incorrecto');
+                }
+            } catch (e) {
+                setError(e);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchAccesibilidades();
+    }, []);
+
+    useEffect(() => {
+        if (onAccesibilidadesChange && Object.keys(accesibilidadesSeleccionadas).length > 0) {
+            onAccesibilidadesChange(accesibilidadesSeleccionadas);
+        }
+    }, [accesibilidadesSeleccionadas, onAccesibilidadesChange]);
+
+
+    const handleCheckboxChange = (id) => {
+        setAccesibilidadesSeleccionadas(prevState => ({
+            ...prevState,
+            [id]: !prevState[id]
+        }));
+    };
+
+    if (loading) {
+        return <div>Cargando accesibilidades...</div>;
+    }
+
+    if (error) {
+        return <div>Error cargando accesibilidades: {error.message}</div>;
+    }
+
+    return (
         <div className="section">
-                <h2>Accesibilidad:</h2>
-
-                <div className="section">
-                    <h4>Saturación:</h4>
-                    <div className="radio-group">
-                        <label><input type="radio" name="saturacion" value="baja"/> Baja</label>
-                        <label><input type="radio" name="saturacion" value="alta"/> Alta</label>
-                    </div>
-                </div>
-
-                <div className="section">
-                    <h4>Contraste:</h4>
-                    <div className="radio-group">
-                        <label><input type="radio" name="contraste" value="bajo"/> Bajo</label>
-                        <label><input type="radio" name="contraste" value="alto"/> Alto</label>
-                    </div>
-                </div>
-
-                <div className="section">
-                    <h4>Daltonismo:</h4>
-                    <div className="radio-group">
-                        <label><input type="radio" name="daltonismo" value="si"/> Sí</label>
-                        <label><input type="radio" name="daltonismo" value="no"/> No</label>
-                    </div>
-                </div>
-
-                <div className="section">
-                    <h4>Visualización:</h4>
-                    <div className="checkbox-group">
-                        <label><input type="checkbox" name="visualizacion" value="pictogramas"/> Pictogramas</label>
-                        <label><input type="checkbox" name="visualizacion" value="texto"/> Texto</label>
-                        <label><input type="checkbox" name="visualizacion" value="videos"/> Videos</label>
-                        <label><input type="checkbox" name="visualizacion" value="imagenes"/> Imágenes</label>
-                        <label><input type="checkbox" name="visualizacion" value="audio"/> Audio</label>
-                    </div>
-                </div>
-
-                <div className="section">
-                    <h4>Navegación:</h4>
-                    <div className="checkbox-group">
-                        <label><input type="checkbox" name="navegacion" value="raton"/> Ratón</label>
-                        <label><input type="checkbox" name="navegacion" value="teclado"/> Teclado</label>
-                        <label><input type="checkbox" name="navegacion" value="pulsador"/> Pulsador</label>
-                    </div>
+            <h2>Accesibilidad:</h2>
+            <div className="section">
+                <h4>Selecciona las accesibilidades:</h4>
+                <div className="checkbox-group">
+                    {accesibilidades.map((accesibilidad) => (
+                        <label key={accesibilidad.id}>
+                            <input
+                                type="checkbox"
+                                name={accesibilidad.nombre}
+                                value={accesibilidad.id}
+                                checked={accesibilidadesSeleccionadas[accesibilidad.id] || false}
+                                onChange={() => handleCheckboxChange(accesibilidad.id)}
+                            />
+                            {accesibilidad.nombre}
+                        </label>
+                    ))}
                 </div>
             </div>
-            
-
-
-        
-        
+        </div>
     );
 }
 
-export default Accesibilidad
+export default Accesibilidad;
--- FrontEnd/src/pages/Alumno_Perfil.jsx
+++ FrontEnd/src/pages/Alumno_Perfil.jsx
@@ -1,55 +1,103 @@
-import '../css/Admin.css'
+import '../css/Admin.css';
 import Boton from '../components/Boton';
 import Perfil from '../components/Perfil';
 import Cabecera from '../components/Cabecera';
 import { useState, useEffect } from 'react';
 import Accesibilidad from '../components/Accesibilidad';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 function AlumnoPerfil() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [alumno, setAlumno] = useState({});
   const [loading, setLoading] = useState(true);
-
-  const URL_IMAGEN_POR_DEFECTO =  "https://pbs.twimg.com/profile_images/1665732637011460097/_HijfwBd_400x400.jpg";
-  ///////////////////////////////////////FALTA GET//////////////////////////////////////////////////////////7
-
-  async function getAlumno() {
-    try {
-      const response = await fetch(`https://especialeduca.jmarin.dev/api/alumnos/${id}`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      setAlumno(data.data); 
-    } catch (error) {
-      console.error("Error al obtener el alumno:", error);
-      setAlumno({});
-    } finally {
-      setLoading(false);
-    }
-  }
+  const [accesibilidadesAlumno, setAccesibilidadesAlumno] = useState({});
+  const URL_IMAGEN_POR_DEFECTO = "https://pbs.twimg.com/profile_images/1665732637011460097/_HijfwBd_400x400.jpg";
 
   useEffect(() => {
+    async function getAlumno() {
+      try {
+        setLoading(true);
+        const response = await fetch(`https://especialeduca.jmarin.dev/api/alumnos/${id}`);
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        const data = await response.json();
+        setAlumno(data.data);
+
+        const accesibilidadesResponse = await fetch(`https://especialeduca.jmarin.dev/api/alumnos/${id}/accesibilidades/`);
+        if (!accesibilidadesResponse.ok) throw new Error(`HTTP error! status: ${accesibilidadesResponse.status}`);
+        const accesibilidadesData = await accesibilidadesResponse.json();
+        if (accesibilidadesData && accesibilidadesData.data) {
+          const accesibilidadesSeleccionadas = {};
+          accesibilidadesData.data.forEach(accesibilidad => {
+            accesibilidadesSeleccionadas[accesibilidad.id] = true;
+          });
+          setAccesibilidadesAlumno(accesibilidadesSeleccionadas);
+        } else {
+          setAccesibilidadesAlumno({});
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setAlumno({});
+      } finally {
+        setLoading(false);
+      }
+    }
+
     getAlumno();
   }, [id]);
 
+  const handleAccesibilidadesChange = (nuevasAccesibilidades) => {
+    setAccesibilidadesAlumno(nuevasAccesibilidades);
+  };
+
+  const guardarAccesibilidades = async () => {
+    try {
+      setLoading(true);
+      const accesibilidadesIds = Object.entries(accesibilidadesAlumno)
+        .filter(([id, isSelected]) => isSelected)
+        .map(([id]) => id);
+
+      const response = await fetch(`https://especialeduca.jmarin.dev/api/alumnos/${id}/accesibilidades/`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ accesibilidades: accesibilidadesIds })
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(`Error al guardar accesibilidades: ${response.status} ${errorData.message || ''}`);
+      }
+
+      navigate(`/alumno_list`);
+
+    } catch (error) {
+      console.error("Error al guardar las accesibilidades:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   if (loading) {
     return <div>Cargando datos del alumno...</div>;
+  }
+
+  if (!alumno) {
+    return <div>No se encontró el alumno.</div>;
   }
 
   return (
     <>
       <Cabecera nombre="Alumno" route="/alumno_list" />
       <div>
-        <Perfil nombre={alumno.nickname} foto={alumno.foto || URL_IMAGEN_POR_DEFECTO} fechaNacimiento={alumno.fechaNacimiento}/>
-        <br/>
-        <Boton nombre="Seguimiento" route="/seguimiento" />
-        
+        <Perfil nombre={alumno.nickname} foto={alumno.foto || URL_IMAGEN_POR_DEFECTO} fechaNacimiento={alumno.fechaNacimiento} />
+        <br />
+        <Boton nombre="Seguimiento" route={`/seguimiento/${id}`} />
       </div>
-      <br/>
-      <Accesibilidad />
-      <Boton nombre="Guardar" />
+      <br />
+      <Accesibilidad initialAccesibilidades={accesibilidadesAlumno} onAccesibilidadesChange={handleAccesibilidadesChange} />
+      <Boton nombre="Guardar" onClick={guardarAccesibilidades} />
     </>
   );
 }
